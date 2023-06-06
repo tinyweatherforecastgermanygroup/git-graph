37e9f5447abfafa6de5d73267cec7a4645d21748
Jean-Luc Tibaux <eUgEntOptIc@outlook.de>
2023-06-06 19:45:32 +0000

    improve formatting

--- __init__.py
+++ __init__.py
@@ -1,21 +1,28 @@
-
 import concurrent.futures
-from multiprocessing import cpu_count
 import json
+import sys
+from multiprocessing import cpu_count
 from pprint import pprint
 
+import requests
 from pydriller import Repository
 
-import requests
+forks_req = requests.get(
+    "https://codeberg.org/api/v1/repos/Starfish/TinyWeatherForecastGermany/forks",
+    timeout=10,
+)
+if forks_req.ok:
+    forks_req_json = forks_req.json()
+else:
+    print(f"failed to request forks -> received unexpected status code {forks_req.status_code} -> text: {forks_req.text}")
+    sys.exit(0)
 
-forks_req = requests.get(f"https://codeberg.org/api/v1/repos/Starfish/TinyWeatherForecastGermany/forks", timeout=10)
-forks_req_json = forks_req.json()
-
-with open('forks-api.json','w+',encoding='utf-8') as fh:
+with open("forks-api.json", "w+", encoding="utf-8") as fh:
     fh.write(str(json.dumps(forks_req_json, indent=4)))
 
-#with open('forks-api.json','r',encoding='utf-8') as fh:
+# with open('forks-api.json','r',encoding='utf-8') as fh:
 #    forks_req_json = json.loads(str(fh.read()))
+
 
 def get_commits_json(clone_url, fork_id):
     print(f"analyzing '{clone_url}' ... ")
@@ -28,30 +35,31 @@ gitGraph
 
     for commit in Repository(clone_url).traverse_commits():
         commit_dict = {}
-        commit_dict['hash'] = str(commit.hash)
-        commit_dict['msg'] = str(commit.msg)
-        commit_dict['author_name'] = str(commit.author.name)
-        #commit_dict['branches'] = commit.branches
+        commit_dict["hash"] = str(commit.hash)
+        commit_dict["msg"] = str(commit.msg)
+        commit_dict["author_name"] = str(commit.author.name)
+        # commit_dict['branches'] = commit.branches
 
-        #commit_dict['mod_files'] = []
-        #for file in commit.modified_files:
+        # commit_dict['mod_files'] = []
+        # for file in commit.modified_files:
         #    commit_dict['mod_files'].append(str(file.filename))
-        
-        mermaid_str += '    commit id:"'+str(commit.hash)[0:6]+'"\n'
+
+        mermaid_str += '    commit id:"' + str(commit.hash)[0:6] + '"\n'
 
         commits_list.append(commit_dict)
         del commit_dict
 
-    with open(f"fork-{fork_id}.json",'w+',encoding='utf-8') as fh:
+    with open(f"fork-{fork_id}.json", "w+", encoding="utf-8") as fh:
         fh.write(str(json.dumps(commits_list, indent=4)))
     del commits_list
 
-    with open(f"fork-{fork_id}.mermaid",'w+',encoding='utf-8') as fh:
+    with open(f"fork-{fork_id}.mermaid", "w+", encoding="utf-8") as fh:
         fh.write(str(mermaid_str))
+
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count()) as executor:
     for fork_entry in forks_req_json:
-        clone_url = str(fork_entry['clone_url'])
+        clone_url = str(fork_entry["clone_url"])
 
-        future = executor.submit(get_commits_json, clone_url, fork_entry['id'])
-        #print(future.result())
+        future = executor.submit(get_commits_json, clone_url, fork_entry["id"])
+        # print(future.result())
